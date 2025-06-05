ce9a25ca0d241eccecccb008e7854997cf73e27c
Doha-Sghir <152553756+Doha-Sghir@users.noreply.github.com>
2025-06-05 13:54:18 +0100

    Change categories

--- frontend/src/components/Category/Category.jsx
+++ frontend/src/components/Category/Category.jsx
@@ -1,12 +1,10 @@
 import React from 'react'
-import Category1 from "../../assets/Category/Leather_Craft_Category.png"
-import Category2 from "../../assets/Category/Ceramics_Category.png"
-import Category3 from "../../assets/Category/Textiles_Category.png"
+import Category1 from "../../assets/Category/Clothing_Category.png"
+import Category2 from "../../assets/Category/Kitchen_Category.png"
+import Category3 from "../../assets/Category/Home_Decor_Category.png"
 import Category4 from "../../assets/Category/Jewelry_Category.png"
-import Category5 from "../../assets/Category/WoodWork_Category.png"
-import Category6 from "../../assets/Category/MetalProd_Category.png"
 import Category7 from "../../assets/Category/Cosmetics_Category.png"
-import Category8 from "../../assets/Category/Spices_Category.png"
+import Category8 from "../../assets/Category/Food_Category.png"
 import CardCategory from './CardCategory'
 
 
@@ -14,23 +12,23 @@ export default function Category() {
   const CategoryInfo = [
     {
       id:1,
-      line1:'Stand Out',
-      line2:'With',
-      line3:'Leather Craft',
+      line1: 'Define',
+      line2: 'Your Look',
+      line3: 'With Timeless Style',
       ImageCategory:Category1,
       delay: 100
     }, {
       id: 2,
-      line1: 'Bring Warmth',
-      line2: 'To Your',
-      line3: 'Ceramics & Pottery',
+      line1: 'Cook Better',
+      line2: 'With',
+      line3: 'Artisan Kitchenware',
       ImageCategory:Category2,
       delay: 200
     }, {
       id:3, 
-      line1: 'Wrap Yourself',
-      line2: 'In',
-      line3: 'Textiles & Carpets',
+      line1: 'Transform',
+      line2: 'Your Space',
+      line3: 'With Elegant Decor',
       ImageCategory:Category3,
       delay: 300
     }, {
@@ -42,32 +40,18 @@ export default function Category() {
       delay: 400
     }, {
       id:5,
-      line1: 'Feel the Craft',
-      line2: 'Of',
-      line3: 'Woodwork',
-      ImageCategory:Category5,
-      delay: 500
-    }, {
-      id:6,
-      line1: 'Timeless Art',
-      line2: 'Forged In',
-      line3: 'Metal Products',
-      ImageCategory:Category6,
-      delay: 600
-    }, {
-      id:7,
       line1: 'Glow Naturally',
       line2: 'With',
       line3: 'Natural Cosmetics',
       ImageCategory:Category7,
-      delay: 700
+      delay: 500
     }, {
-      id:8,
+      id:6,
       line1: 'Spice Up',
       line2: 'Your Taste',
       line3: 'Spices & Food',
       ImageCategory:Category8, 
-      delay: 800
+      delay: 600
     }
   ]
 
@@ -83,7 +67,7 @@ export default function Category() {
       </div>
       <div id="category" className='py-8'>
         <div className='container flex justify-center items-center'>
-          <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-10'>
+          <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-20'>
             {CategoryInfo.map((Category) => (
               <CardCategory key={Category.id} line1={Category.line1} line2={Category.line2} line3={Category.line3} ImageCategory={Category.ImageCategory} delay={Category.delay}/>
             ))}
