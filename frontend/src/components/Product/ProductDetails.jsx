6f64fc8742b7c4135741deb2a79829760b722396
Doha-Sghir <152553756+Doha-Sghir@users.noreply.github.com>
2025-06-18 13:09:17 +0100

    finish product details

--- frontend/src/components/Product/ProductDetails.jsx
+++ frontend/src/components/Product/ProductDetails.jsx
@@ -1,11 +1,14 @@
 import axios from 'axios';
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import { useParams } from 'react-router-dom';
 import ProductDetailsItems from './ProductDetailsItems';
+import { FaCartShopping} from 'react-icons/fa6';
+import Login from '../LoginRegister/Login';
+import Register from '../LoginRegister/Register';
 
 const headVariants = {
     'Home Decor' : {
-        2: ['Height', 'Width', 'Length', 'Color'],
+        2: ['Length', 'Width', 'Height', 'Color'],
         3: ['Size', 'Color']
     },
     'Clothing & Textiles': {
@@ -26,11 +29,84 @@ const headVariants = {
 }
 
 
+
+
 export default function ProductDetails() {
     const { categoryName,id } = useParams();
     const [isLoading, setIsLoading] = useState(true);
     const [product, setProduct] = useState({});
     const [index, setIndex] = useState(0);
+    const [quantity, setQuantity] = useState(1);
+    const [message, setMessage] = useState("");
+    const [messageStatus, setMessageStatus] = useState(false);
+    const [showLogin, setShowLogin] = useState(false);
+    const [showRegister, setShowRegister] = useState(false);
+
+    const addToCart = async (id, quantity) => {
+        const role = localStorage.getItem('role') || '';
+        const access_token = localStorage.getItem('access_token');
+        const element = {
+            product_item_id: id,
+            quantity
+        } 
+
+        
+
+        if(role === 'Customer') {
+            if (!access_token) {
+            setMessage("Unauthorized: You will be logged out. Please log in again.");
+            setMessageStatus(true);
+            setTimeout(() => {
+                localStorage.removeItem("access_token");
+                localStorage.removeItem("refresh_token");
+                localStorage.removeItem("role");
+                localStorage.removeItem("username");
+                localStorage.removeItem("email");
+                localStorage.removeItem("phone");
+                localStorage.removeItem("dob");
+                localStorage.removeItem("gender");
+                window.location.href = '/';
+            }, 2000);
+            
+            return;
+            }
+            try{
+                const res = await axios.post('http://localhost:8000/api/Carts/cart/items/', element,
+                    {
+                        headers: {
+                            Authorization: `Bearer ${access_token}` ,
+                            'Content-Type': 'application/json',
+                        }
+                    });
+                if(res.status === 201) {
+                    setMessage('Item added to cart successfully!');
+                    setMessageStatus(false);
+                }else {
+                    setMessage('Error whene adding this item to the cart.');
+                    setMessageStatus(true);
+                }
+            }catch(error){
+                setMessage(`Error whene adding this item to the cart: ${error}`);
+                setMessageStatus(true);
+            }
+        }else {
+            setMessage('You must be logged in as a Customer to add items.');
+            setMessageStatus(true);
+            setTimeout(() => {
+                setShowLogin(true);
+            }, 2000);
+        }
+
+        setTimeout(() => {
+            setMessage('');
+        }, 3000);
+        
+
+        
+    }
+
+    const modalLoginRef = useRef();
+    const modalRegisterRef = useRef();
 
     const loadProductFromDB = async () => {
         try {
@@ -56,7 +132,6 @@ export default function ProductDetails() {
 
     if (isLoading) return <div>Loading...</div>;
 
-    console.log(product.category === 12 ? headVariants[categoryName] : headVariants[categoryName][product.category]);
     return (
         <div className='flex justify-center mt-5'>
             <div className='my-3 grid grid-cols-1 sm:grid-cols-2 gap-20 w-3/4'>
@@ -74,26 +149,82 @@ export default function ProductDetails() {
                         href={`/Category/${categoryName}`}>See more products for this category</a>
                     <p className='mt-3 text-2xl font-semibold text-primary'>{product?.name ? product.name : null }</p>
                     <p className='mt-2 text-lg text-gray-800 border-b-2 border-gray-500 pb-3'>{product?.description ? product.description : null}</p>
-                    <div className='flex items-end mt-3 px-5 mb-11'>
-                        <p className='text-2xl font-semibold mr-3'>
+                    <div className='grid grid-cols-1 sm:grid-cols-2 gap-5 mt-3 px-5 mb-11'>
+                        <div className='flex items-center'>
+                            <p className='text-2xl font-semibold mr-3'>
                             {Number(product?.items[index]?.price) > product?.items[index]?.price_after_promotion ? 
                                 product.items[index].price_after_promotion
                                 : product?.items[index]?.price}$ 
                             </p>
-                        <p className='line-through text-gray-600 mr-3'>
-                            {Number(product?.items[index]?.price) > product?.items[index]?.price_after_promotion ? 
-                                `${product.items[index].price}$`
-                                : null}
-                        </p>
+                            <p className='line-through text-gray-600 mr-3'>
+                                {Number(product?.items[index]?.price) > product?.items[index]?.price_after_promotion ? 
+                                    `${product.items[index].price}$`
+                                    : null}
+                            </p>
+                            <p className=" text-red-600 font-bold text-lg bg-red-100 px-2 rounded">
+                                {product.active_discount_rate ? `-${parseInt(product.active_discount_rate)}%` : null}
+                            </p>
+                        </div>
+                        <div className='flex items-center'>
+                            <select
+                            value={quantity}
+                            onChange={(e) => setQuantity(e.target.value)}
+                            className="border-2 p-2 m mb-1 rounded-lg border-primary 
+                                        dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white"
+                            >
+                                {[...Array(product?.items[index].stock_quantity).keys()].map(i => (
+                                    <option key={i + 1} value={i + 1}>
+                                    {i + 1}
+                                    </option>
+                                ))}
+                            </select>
+                        </div>
                     </div>
                     <ProductDetailsItems prod={product ? product : null} 
                             headVariants={product.category === 12 ? headVariants[categoryName] : headVariants[categoryName][product.category]}
                             index={index} setIndex={setIndex}
                     /> 
-                       
+                    {/* Order button */}
+                    <button
+                    onClick={() => addToCart(product.items[index].id, quantity)}
+                    className=' bg-gradient-to-r from-primary to-secondary transition-all duration-200
+                                 text-white py-2 mt-8 mb-4 px-4 rounded-full flex items-center gap-3 group
+                                hover:scale-105'
+                    >
+                    <span className='group-hover:block transition-all duration-200'>Add to cart</span>
+                    <FaCartShopping className='text-xl text-white drop-shadow-sm cursor-pointer' />
+                    </button>
+                    <p className={messageStatus ? 'text-primary' : 'text-blue-600'}>{message}</p>
                 
                 </div>
             </div>
+            {showLogin && (
+            <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50">
+                <div ref={modalLoginRef}>
+                <Login
+                    onClose={() => setShowLogin(false)}
+                    onSwitchToRegister={() => {
+                    setShowLogin(false);
+                    setShowRegister(true);
+                    }}
+                />
+                </div>
+            </div>
+            )}
+
+            {showRegister && (
+            <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50">
+                <div ref={modalRegisterRef}>
+                <Register 
+                    onClose={() => setShowRegister(false)} 
+                    onSwitchToLogin={() => {
+                    setShowRegister(false);
+                    setShowLogin(true);
+                    }}
+                />
+                </div>
+            </div>
+            )}
         </div>
         
     );
